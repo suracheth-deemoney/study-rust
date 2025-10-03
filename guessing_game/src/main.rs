0161792c176edc3f49bc2199a002ec3df9c332c9
Suracheth Chawla <suracheth.ch@deemoney.com>
2025-10-03 10:07:52 +0700

    Lopping the program to allow user to reinput the number and break the loop once the number is matched

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,21 +8,27 @@ fn main() {
     let secret_number = rand::rng().random_range(1..=100);
 
     println!("The secret number is: {secret_number}");
-    println!("Please input your guess.");
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("Your guessed: {guess}");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        println!("Your guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
