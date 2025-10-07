dae43df13ce9c7c6b0b94c5740de061942ad8726
Suracheth Chawla <suracheth.ch@deemoney.com>
2025-10-07 10:16:04 +0700

    adjust function to take two arguments

--- function/src/main.rs
+++ function/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    another_function(5);
+    print_label_measurement(5, 'h');
 }
 
-fn another_function(x: i32) {
-    println!("The value of x is: {x}");
+fn print_label_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value} {unit_label}");
 }
