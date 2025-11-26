3be4ca326643308d87003597984809fffa7d613e
Suracheth Chawla <suracheth.chawla@outlook.com>
2025-11-26 23:47:31 +0700

    use derive debug

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,6 +1,7 @@
+#[derive(Debug)]
 struct Rectangle {
-    width: u32,
     height: u32,
+    width: u32,
 }
 
 fn main() {
@@ -8,12 +9,5 @@ fn main() {
         width: 30,
         height: 50,
     };
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("rect1 is {rect1:#?}");
 }
