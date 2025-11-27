3f423e45cd091064144da310c4912b01b6bb16f2
Suracheth Chawla <suracheth.ch@deemoney.com>
2025-11-27 10:48:34 +0700

    Add debug derive and scale factor to Rectangle struct

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,19 +1,15 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
 fn main() {
+    let scale: u32 = 2;
     let rect1 = Rectangle {
-        width: 30,
+        width: dbg!(30 * scale),
         height: 50,
     };
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
-}
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    dbg!(&rect1);
 }
