20b7b043e9bd14c1893b6f2c221dd46da8c22e98
Suracheth Chawla <suracheth.chawla@outlook.com>
2025-12-21 20:38:36 +0700

    Also use `?` on the read_to_string

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -7,10 +7,9 @@ fn main() {
 }
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let mut username_file = File::open("hello.txt")?;
     let mut username = String::new();
 
-    username_file.read_to_string(&mut username);
+    File::open("hello.txt")?.read_to_string(&mut username)?;
 
     Ok(username)
 }
