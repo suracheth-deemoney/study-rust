08686e9efefb7213a91cac18561d97971f2c44d9
Suracheth Chawla <suracheth.chawla@outlook.com>
2025-12-22 16:53:13 +0700

    Use generic on the method

--- generic/src/main.rs
+++ generic/src/main.rs
@@ -15,6 +15,17 @@ struct Point<T, U> {
     y: U,
 }
 
+struct SingleTypePoint<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> SingleTypePoint<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
@@ -29,4 +40,8 @@ fn main() {
     let _point_both_integer = Point { x: 5, y: 10 };
     let _point_both_float = Point { x: 1.0, y: 4.0 };
     let _point_integer_and_float = Point { x: 5, y: 4.0 };
+
+    let p = SingleTypePoint { x: 5, y: 10 };
+
+    println!("p.x = {}", p.x());
 }
