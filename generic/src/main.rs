30df5bfc58717730822f5eb73488e55cae5dc13d
Suracheth Chawla <suracheth.chawla@outlook.com>
2025-12-22 16:38:48 +0700

    Introduce largest_char function after renaming the largest function to largest_i32

--- generic/src/main.rs
+++ generic/src/main.rs
@@ -1,4 +1,16 @@
-fn largest(list: &[i32]) -> &i32 {
+fn largest_i32(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> &char {
     let mut largest = &list[0];
 
     for item in list {
@@ -13,11 +25,11 @@ fn largest(list: &[i32]) -> &i32 {
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest(&number_list);
+    let result = largest_i32(&number_list);
     println!("The largest number is {result}");
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest(&number_list);
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest_char(&char_list);
 
     println!("The largest number is {result}");
 }
