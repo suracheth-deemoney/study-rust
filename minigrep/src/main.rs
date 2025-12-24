1b3fb4ee75f07c16513d41752a0f6d564d1695a0
Suracheth Chawla <suracheth.chawla@outlook.com>
2025-12-24 12:13:33 +0700

    Handling when no. of arguments are not enough

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,4 @@
-use std::env;
-use std::fs;
+use std::{env, fs, process};
 
 struct Config {
     query: String,
@@ -7,18 +6,25 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
