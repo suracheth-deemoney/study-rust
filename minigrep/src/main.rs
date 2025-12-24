89cd5494998abeccc6affdbcff603f892adc85be
Suracheth Chawla <suracheth.chawla@outlook.com>
2025-12-24 12:50:15 +0700

    Remove debug printing from main

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,8 +5,6 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
-
         process::exit(1);
     });
 
@@ -14,7 +12,6 @@ fn main() {
     println!("In file {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
         process::exit(1);
     }
 }
