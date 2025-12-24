38947700d4f13d05355727dd41d7a69e8b09193f
Suracheth Chawla <suracheth.chawla@outlook.com>
2025-12-24 14:27:52 +0700

    Send error to standard error.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,10 +5,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problems parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
