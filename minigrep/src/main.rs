cc8a49d6da1f0c83b648ebf829b9032a3357bb7d
Suracheth Chawla <suracheth.chawla@outlook.com>
2025-12-24 12:06:00 +0700

    Create a constructor function for Config

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,17 +6,19 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    Config {
-        query: args[1].clone(),
-        file_path: args[2].clone(),
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
