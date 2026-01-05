4ae6c5c8299f31a98f86063408ba202f5dfe4b17
Suracheth Chawla <suracheth.chawla@outlook.com>
2026-01-05 21:55:53 +0700

    Auto dererence

--- deref-example/src/main.rs
+++ deref-example/src/main.rs
@@ -16,10 +16,20 @@ impl<T> Deref for MyBox<T> {
     }
 }
 
+fn hello(name: &str) {
+    println!("Hello, {name}!");
+}
+
 fn main() {
     let x = 5;
     let y = MyBox::new(5);
 
     assert_eq!(5, x);
     assert_eq!(5, *y);
+
+    let m = MyBox::new(String::from("Rust"));
+    // This works because MyBox has Deref
+    // and String already has Deref
+    // This is called Deref coersion
+    hello(&m);
 }
