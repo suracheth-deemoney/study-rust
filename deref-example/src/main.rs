cc60d4e2e24824f83c3cd50e5ead892b744f3419
Suracheth Chawla <suracheth.chawla@outlook.com>
2026-01-05 21:43:16 +0700

    Add Deref trait so to be able to do the derefence

--- deref-example/src/main.rs
+++ deref-example/src/main.rs
@@ -1,7 +1,24 @@
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 fn main() {
     let x = 5;
-    // x is actually copied in the below instruction rather than
-    let y = Box::new(x);
+    let y = MyBox::new(5);
 
     assert_eq!(5, x);
     assert_eq!(5, *y);
