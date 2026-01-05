12bd5e970304e8ff4ad1fe33ded164d6163ea346
Suracheth Chawla <suracheth.chawla@outlook.com>
2026-01-05 21:29:04 +0700

    Create Cons datastructure using Box<T>

--- con-list/src/main.rs
+++ con-list/src/main.rs
@@ -1,3 +1,10 @@
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+
 fn main() {
-    println!("Hello, world!");
+    let _list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
 }
