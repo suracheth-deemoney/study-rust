36fb7a8e24dc2369a6902975c235ef2270e19d4c
Suracheth Chawla <suracheth.chawla@outlook.com>
2026-01-07 04:58:40 +0700

    Using with Reference<T>

--- con-list/src/main.rs
+++ con-list/src/main.rs
@@ -1,10 +1,15 @@
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
 use crate::List::{Cons, Nil};
+use std::rc::Rc;
 
 fn main() {
-    let _list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let _b = Cons(3, Rc::clone(&a));
+    // let _c = Cons(4, a.clone());
+    // Rust convension is to use Rc::clone instead of cloning Rc<T>
+    let _c = Cons(4, Rc::clone(&a));
 }
