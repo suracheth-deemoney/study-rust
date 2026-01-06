a508e19dc55609404575f9bd4389eb12167de9cb
Suracheth Chawla <suracheth.chawla@outlook.com>
2026-01-07 04:32:35 +0700

    Add Drop Trait example

---
+++ drop-example/src/main.rs
@@ -0,0 +1,20 @@
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`", self.data);
+    }
+}
+fn main() {
+    let _c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+
+    let _d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+
+    println!("CustomSmartPointer created!");
+}
