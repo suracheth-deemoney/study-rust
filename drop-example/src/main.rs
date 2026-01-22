62bddf51e774f17fe92aed4bafef27283264c882
Suracheth Chawla <suracheth.chawla@outlook.com>
2026-01-22 09:56:05 +0700

    Show drop method and how to use std::mem::drop

--- drop-example/src/main.rs
+++ drop-example/src/main.rs
@@ -1,3 +1,30 @@
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}` !", self.data);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+
+    let e = CustomSmartPointer {
+        data: String::from("some data"),
+    };
+
+    println!("CustomSmartPointer created!");
+
+    // c.drop(); is not allowed
+    drop(c);
+
+    println!("CustomSmartPointer for `c` dropped before the end of main");
 }
