035bbb1f875a1d6b0883b34decb1cb8fb897ac0c
Suracheth Chawla <suracheth.chawla@outlook.com>
2026-01-23 10:04:23 +0700

    Use Reference counting to have multiple owners

--- con-list/src/main.rs
+++ con-list/src/main.rs
@@ -1,12 +1,13 @@
+use std::rc::Rc;
 enum List {
     Nil,
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
 }
 
 use crate::List::{Cons, Nil};
 
 fn main() {
-    let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
-    let b = Cons(3, Box::new(a));
-    // let c = Cons(4, Box::new(a)); Due to `a` is already moved into b
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    let b = Cons(3, Rc::clone(&a));
+    let c = Cons(4, Rc::clone(&a));
 }
