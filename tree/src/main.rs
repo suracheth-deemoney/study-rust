6a79ed45582aa67b1a884f8037d4d679eea15365
Suracheth Chawla <suracheth.chawla@outlook.com>
2026-01-28 11:07:11 +0700

    Use Rc::Weak to for non owning reference

--- tree/src/main.rs
+++ tree/src/main.rs
@@ -1,22 +1,29 @@
-use std::cell::RefCell;
-use std::rc::Rc;
+use std::cell::{RefCell, RefMut};
+use std::rc::{Rc, Weak};
 
 #[derive(Debug)]
 struct Node {
     value: i32,
     children: RefCell<Vec<Rc<Node>>>,
+    parent: RefCell<Weak<Node>>,
 }
 
 fn main() {
     let leaf = Rc::new(Node {
         value: 3,
         children: RefCell::new(vec![]),
+        parent: RefCell::new(Weak::new()),
     });
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 
     let branch = Rc::new(Node {
         value: 5,
         children: RefCell::new(vec![Rc::clone(&leaf)]),
+        parent: RefCell::new(Weak::new()),
     });
 
-    println!("{:?}", branch);
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 }
