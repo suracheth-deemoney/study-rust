c0a8b8d2417bbf53a57ac1f3a9137486f8778166
Suracheth Chawla <suracheth.chawla@outlook.com>
2026-01-29 09:55:03 +0700

    use of move

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,18 +1,11 @@
 use std::thread;
-use std::time::Duration;
 
 fn main() {
-    let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {i} from spawed thread!");
-            thread::sleep(Duration::from_millis(1));
-        }
-    });
+    let v = vec![1, 2, 3];
 
-    for i in 1..5 {
-        println!("hi number {i} from the main thread!");
-        thread::sleep(Duration::from_millis(1));
-    }
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {v:?}");
+    });
 
     handle.join().unwrap();
 }
