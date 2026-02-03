10736da956add523c8eaf7968bf07ceb70481734
Suracheth Chawla <suracheth.chawla@outlook.com>
2026-02-03 13:02:33 +0700

    use Arc and Mutex for sharing

--- shared-state/src/main.rs
+++ shared-state/src/main.rs
@@ -1,12 +1,26 @@
-use std::sync::Mutex;
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+};
 
 fn main() {
-    let m = Mutex::new(5);
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
 
-    {
-        let mut num = m.lock().unwrap();
-        *num = 6;
+    for _ in 0..1000 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+
+        handles.push(handle);
     }
 
-    println!("m = {m:?}");
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
