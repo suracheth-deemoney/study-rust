987d884b9d736cf0f8be9ecaeac50ed5749271eb
Suracheth Chawla <suracheth.chawla@outlook.com>
2026-02-05 21:39:21 +0700

    Use tprl's race to race between two futures

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -1,24 +1,32 @@
 use std::env;
-use trpl::Html;
+use trpl::{Either, Html};
 
-async fn page_title(url: &str) -> Option<String> {
-    let response = trpl::get(url).await;
-    let response_text = response.text().await;
-    // Or response_text = trpl::get(url).await.text().await;
-
-    Html::parse(&response_text)
+async fn page_title(url: &str) -> (&str, Option<String>) {
+    let text = trpl::get(url).await.text().await;
+    let title = Html::parse(&text)
         .select_first("title")
-        .map(|title_element| title_element.inner_html())
+        .map(|title| title.inner_html());
+
+    (url, title)
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     trpl::run(async {
-        let url = &args[1];
-        match page_title(url).await {
-            Some(title) => println!("the title for {url} was {title}"),
-            None => println!("{url} had no title"),
+        let title_fut_1 = page_title(&args[1]);
+        let title_fut_2 = page_title(&args[2]);
+
+        let (url, maybe_title) = match trpl::race(title_fut_1, title_fut_2).await {
+            Either::Left(left) => left,
+            Either::Right(right) => right,
+        };
+
+        println!("{url} returned first");
+
+        match maybe_title {
+            Some(title) => println!("Its page title is '{title}'"),
+            None => println!("Its title could not be parsed."),
         }
     })
 }
